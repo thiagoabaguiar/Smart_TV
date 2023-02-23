4a70e70ebaa766cd4de0c62daba211b446282861
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-02-22 22:39:13 -0300

    Methods created

--- src/smartTV.java
+++ src/smartTV.java
@@ -1,5 +1,38 @@
 public class smartTV {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    boolean statusTV;
+    int channelSelected;
+    int volumeSelected;
+
+    public smartTV() {
+        statusTV = false;
+        channelSelected = 0;
+        volumeSelected = 0;
+    }    
+
+    public void changeStatusTV(boolean statusTV) {
+        statusTV = !statusTV;
     }
+    
+    public void upVolume(int volumeSelected){
+        volumeSelected ++;
+    }
+
+    public void downVolume(int volumeSelected){
+        volumeSelected --;
+    }
+    
+    public void upChannel(int channelSelected){
+        channelSelected ++;
+    }
+    
+    public void downChannel(int channelSelected){
+        channelSelected --;
+    }
+    
+    public int setChannel(int channelSelected){
+        int newChannelSelected = channelSelected;
+        return newChannelSelected;
+    }        
+
 }
